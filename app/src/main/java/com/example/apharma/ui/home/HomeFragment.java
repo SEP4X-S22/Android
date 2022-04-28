e192453551915650df284848af4e8722297f78ed
Valeriu1 <roscavaleriuvaleriu@gmail.com>
2022-04-28 17:45:50 +0200

    Sensors fragment added, navigation also possible

--- app/src/main/java/com/example/apharma/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/apharma/ui/home/HomeFragment.java
@@ -1,28 +1,51 @@
 package com.example.apharma.ui.home;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.NavController;
+import androidx.navigation.NavHostController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 
+import com.example.apharma.R;
 import com.example.apharma.databinding.FragmentHomeBinding;
 
-public class HomeFragment extends Fragment {
+public class HomeFragment extends Fragment   {
 
     private FragmentHomeBinding binding;
+    private ImageButton deviceButton;
+    private NavController navController;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         HomeViewModel homeViewModel =
                 new ViewModelProvider(this).get(HomeViewModel.class);
 
+
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+
+
+        deviceButton = binding.imageButton;
+
+
+        deviceButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Navigation.findNavController(view).navigate(R.id.action_navigation_home_to_sensors);
+            }
+        });
+
+
 
 
         return root;
