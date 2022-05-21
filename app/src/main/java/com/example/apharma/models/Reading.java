ac7a3fda1cf2029f5a5dc747d2a360d32f27b349
Lori <loredanacicati@gmail.com>
2022-05-21 13:22:33 +0200

    update to String date (Reading model class)

--- app/src/main/java/com/example/apharma/models/Reading.java
+++ app/src/main/java/com/example/apharma/models/Reading.java
@@ -1,20 +1,17 @@
 package com.example.apharma.models;
 
-import java.util.Date;
-
 public class Reading {
 
     private int id;
     private double readingValue;
 
-    private Date timeStamp;
+    private String timeStamp;
 
-    public Reading()
-    {
+    public Reading() {
     }
 
-    @Override public boolean equals(Object o)
-    {
+    @Override
+    public boolean equals(Object o) {
         if (this == o)
             return true;
         if (o == null || getClass() != o.getClass())
@@ -24,45 +21,38 @@ public class Reading {
                 && timeStamp.equals(reading.timeStamp);
     }
 
-    public Reading(int readingValue, Date timeStamp)
-    {
+    public Reading(int readingValue, String timeStamp) {
         this.readingValue = readingValue;
         this.timeStamp = timeStamp;
     }
 
-    @Override public String toString()
-    {
+    @Override
+    public String toString() {
         return "Reading{" + "id=" + id + ", readingValue=" + readingValue
                 + ", timeStamp=" + timeStamp + '}';
     }
 
-    public int getId()
-    {
+    public int getId() {
         return id;
     }
 
-    public void setId(int id)
-    {
+    public void setId(int id) {
         this.id = id;
     }
 
-    public double getReadingValue()
-    {
+    public double getReadingValue() {
         return readingValue;
     }
 
-    public void setReadingValue(int readingValue)
-    {
+    public void setReadingValue(int readingValue) {
         this.readingValue = readingValue;
     }
 
-    public Date getTimeStamp()
-    {
+    public String getTimeStamp() {
         return timeStamp;
     }
 
-    public void setTimeStamp(Date timeStamp)
-    {
+    public void setTimeStamp(String timeStamp) {
         this.timeStamp = timeStamp;
     }
 }
